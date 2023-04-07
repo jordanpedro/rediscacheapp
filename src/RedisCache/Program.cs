20b2f0dd0d742c51ca1c259c7724f728b2d150fa
jordan ricardo pedro <jordan.pedro@viavarejo.com.br>
2023-04-06 22:06:57 -0300

    simple application commit

---
+++ src/RedisCache/Program.cs
@@ -0,0 +1,15 @@
+﻿using RedisCache;
+
+var cache = RedisHelper.Database;
+await cache.StringSetAsync("OURKEY", "JORDAN");
+
+var value = await cache.StringGetAsync("OURKEY");
+
+for(int a = 0; a < 100; a++)
+{
+    await cache.StringSetAsync($"OURKEY_{a}", $"JORDAN_{a}");
+}
+for (int a = 0; a < 100; a++)
+{
+    Console.WriteLine( await cache.StringGetAsync($"OURKEY_{a}"));
+}
---
+++ src/RedisCache/RedisHelper.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisCache
+{
+    internal class RedisHelper
+    {
+        private static IDatabase redisDatabase;
+        public static IDatabase Database
+        {
+            get
+            {
+                return redisDatabase;
+            }
+        }
+        static RedisHelper()
+        {
+            var connection = ConnectionMultiplexer.Connect("localhost:6379");
+            redisDatabase = connection.GetDatabase();
+        }
+    }
+}
